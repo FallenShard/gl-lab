350fda3fd6a016e1a9ff808573b60987aa13c5db
FallenShard <nemanjabartolovic@yahoo.com>
2014-12-18 14:16:46 +0100

    Fix lab 1

---
+++ GL-Template/vec_types.h
@@ -0,0 +1,31 @@
+#pragma once;
+
+typedef struct
+{
+    float x;
+    float y;
+    float z;
+} vec3;
+
+typedef struct
+{
+    float x;
+    float y;
+    float z;
+    float w;
+} vec4;
+
+typedef struct
+{
+    unsigned short a;
+    unsigned short b;
+    unsigned short c;
+    unsigned short d;
+} usvec4;
+
+typedef struct
+{
+    unsigned short a;
+    unsigned short b;
+    unsigned short c;
+} usvec3;
